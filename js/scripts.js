d41928f45fdec3d416ca8f338226a90a76e6de36
melindawright <melindawright@Melindas-MacBook-Air.local>
2014-05-21 14:18:56 -0700

    Version 1

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+
+	$("#main").menutron({
+		menuTitle: "Eat Sushi"
+	});
+  
+  // Your Scripts
+
+});
+
